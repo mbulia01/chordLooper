7ac0c142d457dd6e7b4d2f030351c95464e74a3d
Max Bulian <maxbulian@gmail.com>
2012-07-09 18:45:07 -0400

    Initial Commit

---
+++ tufts/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  tufts
+//
+//  Created by Chris Beltis on 7/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
