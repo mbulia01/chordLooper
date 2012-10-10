563d26238028e368be43f5f966a1926af4fc993b
Max Bulian <maxbulian@gmail.com>
2012-10-10 14:42:14 -0400

    added pause/play button

--- tufts/ViewController.h
+++ tufts/ViewController.h
@@ -7,7 +7,26 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource, AVAudioPlayerDelegate>
+{
+    NSMutableArray *chords; //contains chords that can be chosen
+    NSMutableArray *audioQueue; //contains chords that will be played in order
+    NSArray *chordsInitArray;
+    NSArray *audioQueueInit;
+    IBOutlet UIPickerView *pickerView;
+    IBOutlet UIButton *currentButton;
+    AVAudioPlayer *audioPlayer;
+    NSUInteger index;
+    bool isPlaying;
+}
+
+@property (nonatomic, readonly, retain) NSString *currentTitle;
+@property(readonly, getter=isPlaying) BOOL playing;
+
+-(IBAction)chooseChord:(id)sender;
+-(IBAction)playOrPause:(id)sender;
+
 
 @end
--- tufts/ViewController.m
+++ tufts/ViewController.m
@@ -8,16 +8,89 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
-@end
-
 @implementation ViewController
+
+@synthesize currentTitle;
+@synthesize playing;
+
+-(IBAction)chooseChord:(id)sender
+{   
+    currentButton = sender;
+    currentButton.titleLabel.font = [UIFont systemFontOfSize:36];
+    NSInteger pickerIndex = [pickerView selectedRowInComponent:0];
+    NSString *title = [chords objectAtIndex:pickerIndex];
+    [sender setTitle:title forState:UIControlStateNormal&UIControlStateHighlighted&UIControlStateSelected];
+    [audioQueue removeObjectAtIndex:currentButton.tag];
+    [audioQueue insertObject:title atIndex:currentButton.tag];
+}
+
+-(IBAction)playOrPause:(id)sender
+{   
+    if (isPlaying) {
+        [sender setImage:[UIImage imageNamed:@"play.png"] forState:UIControlStateNormal];
+        [audioPlayer stop];
+        isPlaying = false;
+    }
+    else if ([audioQueue objectAtIndex:0] != @"") {
+        [sender setImage:[UIImage imageNamed:@"pause.png"] forState:UIControlStateNormal];
+        [self play];
+    }
+}
+
+-(void)play
+{
+    while ([audioQueue objectAtIndex:index] == @"") {
+        index++;
+        if (index > 7)
+        {
+            index = 0;
+        }
+    }
+    //button = (UIButton*)[self.view viewWithTag:index];
+    //[self changeColor:button :[UIImage imageNamed:@"atbnss0354.gif"]];
+    NSURL *url = [NSURL fileURLWithPath:[NSString stringWithFormat:@"%@/%@.mp3", [[NSBundle mainBundle] resourcePath], [audioQueue objectAtIndex:index]]];
+    NSError *error;
+    audioPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+    [audioPlayer setDelegate:self];
+    [audioPlayer play];
+    isPlaying = true;
+}
+
+-(void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag
+{
+    index++;
+    if (index > 7)
+    {
+        index = 0;
+    }
+    isPlaying = false;
+    [self play];
+}
+
+-(void)changeColor:(UIButton*)button:(UIImage*)image
+{
+    [button setImage:image forState:UIControlStateNormal];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background.jpg"]];
+    
+    chords = [[NSMutableArray alloc] init];
+    chordsInitArray = [[NSArray alloc] init];
+	chordsInitArray = [NSArray arrayWithObjects: @"A",@"Am",@"A#",@"A#m",@"B",@"Bm",@"C",@"Cm",@"C#",@"C#m",@"D",@"Dm",@"D#",@"D#m",@"E",@"Em",@"F",@"Fm",@"F#",@"F#m",@"G",@"Gm",@"G#",@"G#m",nil];
+    [chords addObjectsFromArray:chordsInitArray];
+    
+    audioQueue = [[NSMutableArray alloc] initWithCapacity:8];
+    audioQueueInit = [[NSArray alloc] init];
+    audioQueueInit = [NSArray arrayWithObjects:@"",@"",@"",@"",@"",@"",@"",@"",nil];
+    [audioQueue addObjectsFromArray:audioQueueInit];
+    
+    [pickerView selectRow:1 inComponent:0 animated:NO];
+    
+    index = 0;
+    isPlaying = false;
 }
 
 - (void)viewDidUnload
@@ -26,13 +99,24 @@
     // Release any retained subviews of the main view.
 }
 
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView;
+{
+	return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component;
+{
+	return [chords count];
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component;
+{
+	return [chords objectAtIndex:row];
+}
+
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
+    return UIInterfaceOrientationIsLandscape(interfaceOrientation);
 }
 
 @end
